cbb890dcdf713559bb60c2c671b28fd5ee0d1227
absolutelydawn <absolutelydawnn@gmail.com>
2023-08-25 03:46:55 +0900

    조금더수정

--- bokdukbang/authentication/urls.py
+++ bokdukbang/authentication/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import DuksungLoginView
+from .views import DuksungLoginView, ChooseLocationView
 
 app_name = 'authentication'
 urlpatterns = [
     path('login/', DuksungLoginView.as_view(), name='login'),
-    path('logout/', DuksungLoginView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('chooselocation/', ChooseLocationView.as_view(), name='choose_location'),
 ]
-
-
--- bokdukbang/authentication/views.py
+++ bokdukbang/authentication/views.py
@@ -3,6 +3,13 @@ from .forms import DuksungEmailAuthenticationForm
 
 class DuksungLoginView(LoginView):
     form_class = DuksungEmailAuthenticationForm
+    template_name = 'authentication/login.html'
+    success_url = '/chooselocation/'
 
+# 로그인 성공하면 지역선택
+from django.views.generic import TemplateView
+
+class ChooseLocationView(TemplateView):
+    template_name = 'chooselocation.html'
 
 
